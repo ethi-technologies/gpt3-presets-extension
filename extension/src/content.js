f0f30792a8f3ce4ccbe8f27f205132bea3cb767d
Radu Spineanu <radu.spineanu@gmail.com>
2020-07-04 11:13:07 -0700

    tailwind in ext

--- extension/src/content.js
+++ extension/src/content.js
@@ -3,13 +3,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import root from 'react-shadow';
-
 import App from './App';
+import tailwindcss from './tailwind.css';
 
 class Main extends React.Component {
   render() {
     return (
       <root.div>
+        <style>{tailwindcss}</style>
         <App/> 
       </root.div>
     )
@@ -23,15 +24,27 @@ app.style.display = "none";
 document.body.appendChild(app);
 ReactDOM.render(<Main />, app)
 
+
 const triggerWait = setInterval(() => {
-  const triggerContainer = document.querySelector(".title-wrapper .title-section:last-child");
+  const triggerContainer = document.querySelector(".title-wrapper");
+
   if (triggerContainer) {
     clearInterval(triggerWait);
-    triggerContainer.innerHTML += "<a href='javascript:void(0)' class='community-presets' style='margin-left: 5px; color: rgba(255,57,90,.82);'>Community presets</button>";
+    const triggerContainerPosition = triggerContainer.getBoundingClientRect();
+    const top = triggerContainerPosition.y + 10;
+    const left = parseInt(triggerContainerPosition.x + triggerContainerPosition.width) / 2 - 25;
+
+    const trigger = document.createElement('div');
+    trigger.style.cssText = `position: absolute; top: ${top}px; left: ${left}px;`;
+    trigger.innerHTML = `
+    <div style='position: relative'>
+      <a href='javascript:void(0)' class='community-presets' style='margin-left: 5px; color: rgba(255,57,90,.82);'>Community presets</a>
+    </div>`;
+    document.body.appendChild(trigger);
 
     const presetsLink = document.querySelector(".community-presets");
     presetsLink.onclick = () => {
-      app.style.display = "block";
+      document.querySelector("#gpt3-presents").style.display = "block";
     }
   }
 }, 100);
